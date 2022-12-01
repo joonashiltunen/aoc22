7f3456e5f9451c1863f604999d4fed69b0f7b191
Joonas Hiltunen <joonas.hiltunen@gmail.com>
2022-12-01 23:35:17 +0200

    [1] [Refactor] Add wrapper function to make calls prettier

--- fsharp/1b.fs
+++ fsharp/1b.fs
@@ -7,15 +7,17 @@ let f x =
 
 let ints = lines |> Seq.map(f) |> Seq.toList
 
-let rec sums input (current:int) (biggest:list<int>)  = 
-    match input with
-    | [] -> biggest
-    | head::tail ->
-        match head with
-        | Some x -> sums tail (current + x) biggest
-        | None -> sums tail 0 (current::biggest)
+let calculateSums input =
+    let rec sums input (current:int) (biggest:int list)  = 
+        match input with
+        | [] -> biggest
+        | head::tail ->
+            match head with
+            | Some x -> sums tail (current + x) biggest
+            | None -> sums tail 0 (current::biggest)
+    sums input 0 []
 
-let all = sums ints 0 []
+let all = calculateSums ints
 let sorted = List.sortBy(fun x -> -x) all
 
 let result = sorted[0] + sorted[1] + sorted[2]
