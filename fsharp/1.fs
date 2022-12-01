c0be585d63420d08014d03a203c11383b4b422b0
Joonas Hiltunen <joonas.hiltunen@gmail.com>
2022-12-01 22:40:56 +0200

    [1] Add first ever F# solution
    No entry point, can be run using the interactive console.

--- fsharp/1.fs
+++ fsharp/1.fs
@@ -1,1 +1,18 @@
-﻿
+﻿let lines = System.IO.File.ReadLines("1.input");;
+
+let f x = 
+    match x with 
+        | "" -> None
+        | x -> Some(int x)
+
+let ints = lines |> Seq.map(f) |> Seq.toList
+
+let rec biggestsum input (current:int) (biggest:int)  = 
+    match input with
+    | [] -> biggest
+    | head::tail ->
+        match head with
+        | None -> biggestsum tail 0 (max biggest current)
+        | Some x -> biggestsum tail (current + x) biggest
+
+let result = biggestsum ints 0 0
