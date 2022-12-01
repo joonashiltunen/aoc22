9ab0c83f5e19ae0a4d0dab1f1e9bd61e62769f42
Joonas Hiltunen <joonas.hiltunen@gmail.com>
2022-12-01 23:24:59 +0200

    [1] Add F# solution for the second part

---
+++ fsharp/1b.fs
@@ -0,0 +1,21 @@
+﻿let lines = System.IO.File.ReadLines("1.input");;
+
+let f x = 
+    match x with 
+        | "" -> None
+        | x -> Some(int x)
+
+let ints = lines |> Seq.map(f) |> Seq.toList
+
+let rec sums input (current:int) (biggest:list<int>)  = 
+    match input with
+    | [] -> biggest
+    | head::tail ->
+        match head with
+        | Some x -> sums tail (current + x) biggest
+        | None -> sums tail 0 (current::biggest)
+
+let all = sums ints 0 []
+let sorted = List.sortBy(fun x -> -x) all
+
+let result = sorted[0] + sorted[1] + sorted[2]
