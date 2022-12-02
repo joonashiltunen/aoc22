5b9942bab5ea5e757102592ce13c7ffce9a2c9ce
Joonas Hiltunen <joonas.hiltunen@gmail.com>
2022-12-02 08:01:08 +0200

    [2] Add quickly written Python solution for second part

---
+++ py/2b.py
@@ -0,0 +1,17 @@
+def game_score(game):
+    them = game[0]
+    goal = game[1]
+    A_offset = ord("A")
+    X_offset = ord("X") - ord("A") + A_offset
+    them = ord(them) - A_offset
+    goal = ord(goal) - X_offset # 0 lose 1 draw 2 win
+    if goal == 1:
+        return them + 1 + 3
+    if goal == 2:
+        return (them + 1) % 3 + 1 + 6
+    return (them - 1) % 3 + 1
+
+with open("2.input") as f:
+    lines = [line.strip().split() for line in f.readlines()]
+
+print(sum(map(game_score, lines)))
