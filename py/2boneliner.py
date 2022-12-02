d77343ce4f2cbf5d0959105b0e0ea05daff6e772
Joonas Hiltunen <joonas.hiltunen@gmail.com>
2022-12-02 16:05:29 +0200

    [2] [Refactor] Remove unnecessary parentheses

--- py/2boneliner.py
+++ py/2boneliner.py
@@ -1,1 +1,1 @@
-print(sum(map(lambda g:(lambda x,y:(3*y+(x+y-1)%3+1))(ord(g[0])-65,ord(g[1])-88),[l.split()for l in open("2").readlines()])))
+print(sum(map(lambda g:(lambda x,y:3*y+(x+y-1)%3+1)(ord(g[0])-65,ord(g[1])-88),[l.split()for l in open("2").readlines()])))
