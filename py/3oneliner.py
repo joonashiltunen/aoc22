1952dc64f88736baf9397b9df0e6fc3b345377db
Joonas Hiltunen <joonas.hiltunen@gmail.com>
2022-12-03 09:20:03 +0200

    [3] [Refactor] Change oneliners to use pop instead of list conversion to save one character
    Tried to do initially but probably just had a typo.

--- py/3oneliner.py
+++ py/3oneliner.py
@@ -1,1 +1,1 @@
-print(sum(map(lambda r:r>96 and r-96 or r-38,[ord(list(set(l[:len(l)//2])&set(l[len(l)//2:]))[0])for l in open("3").readlines()])))
+print(sum(map(lambda r:r>96 and r-96 or r-38,[ord((set(l[:len(l)//2])&set(l[len(l)//2:])).pop())for l in open("3").readlines()])))
