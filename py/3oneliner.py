0857af76bfebe6651a71f2fbd17eace6ed877b11
Joonas Hiltunen <joonas.hiltunen@gmail.com>
2022-12-04 15:56:14 +0200

    [3] [Shorten] Use min() instead of .pop(), saving 3 characters
    Works because the set is guaranteed to only have one item here.
    
    Thanks to this StackOverflow user's comment which I stumbled upon:
    'Also if you're just aiming for "least code" (which is dumb), then min(s) uses even fewer characters while being just as terrible and inefficient as this. – augurar Jul 16, 2018 at 22:06'

--- py/3oneliner.py
+++ py/3oneliner.py
@@ -1,1 +1,1 @@
-print(sum(map(lambda r:r>96 and r-96 or r-38,[ord((set(l[:len(l)//2])&set(l[len(l)//2:])).pop())for l in open("3").readlines()])))
+print(sum(map(lambda r:r>96 and r-96 or r-38,[ord(min(set(l[:len(l)//2])&set(l[len(l)//2:])))for l in open("3").readlines()])))
