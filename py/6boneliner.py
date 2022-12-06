5e67f5a77d4da4ab50d222417e71f506218184dc
Joonas Hiltunen <joonas.hiltunen@gmail.com>
2022-12-06 10:18:25 +0200

    [6] [Shorten] Make assumption about filesize and take performance hit to save lots of code
    To be more exact, the filesize may still be longer, as long as the result is found within the first 9**9 characters (387.4 MB). Adding one more character of code can extend this to 9**99 characters (2.951e88 MB) for further future-proofing.

--- py/6boneliner.py
+++ py/6boneliner.py
@@ -1,1 +1,1 @@
-(lambda l:[len(set(l[i-14:i]))==14 and exit(str(i))for i in range(len(l))])(open("6").read())
+[len(set(open("6").read()[i-14:i]))==14 and exit(str(i))for i in range(9**9)]
--- py/6oneliner.py
+++ py/6oneliner.py
@@ -1,1 +1,1 @@
-(lambda l:[len(set(l[i-4:i]))==4 and exit(str(i))for i in range(len(l))])(open("6").read())
+[len(set(open("6").read()[i-4:i]))==4 and exit(str(i))for i in range(9**9)]
