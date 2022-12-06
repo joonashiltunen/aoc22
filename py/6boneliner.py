1cb03014e9345e9d808dcb91e5e03e366ee7eade
Joonas Hiltunen <joonas.hiltunen@gmail.com>
2022-12-06 10:51:37 +0200

    [6] [Shorten] Save one character by replacing list comprehension with just for loop
    Slightly less pleasing unfortunately.

--- py/6boneliner.py
+++ py/6boneliner.py
@@ -1,1 +1,1 @@
-[len(set(open("6").read()[i-14:i]))==14 and exit(str(i))for i in range(9**9)]
+for i in range(9**9):len(set(open("6").read()[i-14:i]))==14 and exit(str(i))
--- py/6oneliner.py
+++ py/6oneliner.py
@@ -1,1 +1,1 @@
-[len(set(open("6").read()[i-4:i]))==4 and exit(str(i))for i in range(9**9)]
+for i in range(9**9):len(set(open("6").read()[i-4:i]))==4 and exit(str(i))
