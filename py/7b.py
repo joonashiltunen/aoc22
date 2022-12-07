56884a5513224bb2e4c24a830940d4f5e19a80e8
Joonas Hiltunen <joonas.hiltunen@gmail.com>
2022-12-07 22:44:24 +0200

    [7] [Redesign] Calculate dir sizes of all parents while parsing

--- py/7b.py
+++ py/7b.py
@@ -2,7 +2,7 @@ class Dir():
     def __init__(self, name, parent):
         self.name = name
         self.parent = parent
-        self.size = 0 # ONLY files on this exact level
+        self.size = 0
         self.children = []
     
 dirs = [] # yeah this is probably horribly unnecessary and ugly
@@ -16,20 +16,20 @@ for line in open("7.input").readlines()[1:]: # skip first line with [1:]
         else:
             current_dir = next(c for c in current_dir.children if c.name == d_str)
     elif line[0].isdigit():
-        current_dir.size += int(line.split()[0])
+        dir_size_to_increase = current_dir
+        while dir_size_to_increase:
+            dir_size_to_increase.size += int(line.split()[0])
+            dir_size_to_increase = dir_size_to_increase.parent
     elif line.startswith("dir"):
         new_dir = Dir(line.split()[-1], current_dir)
         current_dir.children.append(new_dir)
         dirs.append(new_dir)
-                
-def dir_size(d):
-    return sum([dir_size(c) for c in d.children]) + d.size
 
-result = root_size = dir_size(root)
+result = root_size = root.size
 free_space_needed = 30000000
 free_space = 70000000-root_size
 for d in dirs:
-    d_size = dir_size(d)
+    d_size = d.size
     if d_size > free_space_needed - free_space and d_size < result:
         result = d_size
 print(result)
