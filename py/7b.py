7dbdef9989fb80af657c4a70e826b1c77dfab88d
Joonas Hiltunen <joonas.hiltunen@gmail.com>
2022-12-07 22:05:08 +0200

    [7] [Refactor] Remove unnecessary iter()
    This was supposed to be for somehow getting the lines within ls, but that was totally unnecessary.

--- py/7b.py
+++ py/7b.py
@@ -12,41 +12,37 @@ class Dir():
 dirs = [] # yeah this is probably horribly unnecessary and ugly
 current_dir = root = Dir("/", None)
 
-I = iter(open("7.input").readlines())
-next(I) # skip first line with cd /
-try:
-    while line := next(I):
-        if line[0] == "$":
-            if line[2:4] == "cd":
-                d_str = line.split()[-1]
-                if d_str == "..":
-                    current_dir = current_dir.parent
-                else:
-                    for c in current_dir.children:
-                        if c.name == d_str:
-                            current_dir = c
-        elif line[0] != "d": # not dir -> size
-            current_dir.size += int(line.split()[0])
-        elif line[0] == "d": # dir
-            new_dir = Dir(line.split()[-1], current_dir)
-            current_dir.children.append(new_dir)
-            dirs.append(new_dir)
+for line in open("7.input").readlines()[1:]: # skip first line with [1:]
+    if line[0] == "$":
+        if line[2:4] == "cd":
+            d_str = line.split()[-1]
+            if d_str == "..":
+                current_dir = current_dir.parent
+            else:
+                for c in current_dir.children:
+                    if c.name == d_str:
+                        current_dir = c
+    elif line[0] != "d": # not dir -> size
+        current_dir.size += int(line.split()[0])
+    elif line[0] == "d": # dir
+        new_dir = Dir(line.split()[-1], current_dir)
+        current_dir.children.append(new_dir)
+        dirs.append(new_dir)
                 
-except StopIteration:
-    def dir_size(d):
-        return sum([dir_size(c) for c in d.children]) + d.size
-    
-    root_size = dir_size(root)
-    
-    
-    free_space_needed = 30000000
-    free_space = 70000000-root_size
-    result = root_size
-    for d in dirs:
-        d_size = dir_size(d)
-        if d_size > free_space_needed - free_space and d_size < result:
-            result = d_size
-    print(result)
+def dir_size(d):
+    return sum([dir_size(c) for c in d.children]) + d.size
+
+root_size = dir_size(root)
+
+
+free_space_needed = 30000000
+free_space = 70000000-root_size
+result = root_size
+for d in dirs:
+    d_size = dir_size(d)
+    if d_size > free_space_needed - free_space and d_size < result:
+        result = d_size
+print(result)
             
             
     
