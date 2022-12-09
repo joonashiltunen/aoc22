882ee6e0df0306748bcd2059193f3f033e0e4e0b
Joonas Hiltunen <joonas.hiltunen@gmail.com>
2022-12-10 00:07:05 +0200

    [9] Clean solutions
    All the if/elif is obviously unnecessary. The toofar function remains slightly mysterious.

--- py/9.py
+++ py/9.py
@@ -1,48 +1,22 @@
-visited = [] # set()
 dirs = {"R": (1, 0), "L": (-1, 0), "U": (0, 1), "D": (0, -1)}
 
 vecsum = lambda v1, v2: (v1[0]+v2[0], v1[1]+v2[1])
-def vecdist(v1, v2):
-    xd = v2[0]-v1[0]
-    yd = v2[1]-v1[1]
-    return (xd, yd)
-    
-def vecabs(v):
-    return (abs(v[0]), abs(v[1]))
-   
-def toofar(v): # returns 1 for 2-space normal case, 2 for diagonal move required
-    v = vecabs(v)
-    if v[0]==v[1]: return 1
-    return sum(v)
+vecdist = lambda v1, v2: (v2[0]-v1[0], v2[1]-v1[1])
 
-#test
-h = (4,2)
-t = (3,0)
-print(toofar(vecdist(h,t)))
+def toofar(dist):
+    v = abs(dist[0]), abs(dist[1])
+    if v[0]==v[1]: 
+        return False
+    return sum(v) > 1
 
-
-H = (0, 0)
-T = (0, 0)
+H = T = (0, 0)
+visited = set()
 for line in open("9.input").readlines():
-    d = line[0]
-    print(d)
-    moves = int(line.strip()[2:])
-    for i in range(moves):
-        H = vecsum(H, dirs[d])
-        if f := toofar(vecdist(T, H)):
-            #if f == 1:
-                #T = vecsum(T, dirs[d])
-            if f > 1:
-                x,y=vecdist(T, H)
-                if x==0 or y==0:
-                    T = vecsum(T, dirs[d])
-                elif abs(x)==1 and abs(y)==2:
-                    T = vecsum(T, (x, abs(y)//y))
-                elif abs(x)==2 and abs(y)==1:
-                    T = vecsum(T, (abs(x)//x, y))
-                else:
-                    raise Exception("unexpected diag move")
-            visited.append(T)
-        print(H, T)
-   
-print(len(set(visited)))
+    for _move in range(int(line.strip()[2:])):
+        H = vecsum(H, dirs[line[0]])
+        if toofar(vecdist(T, H)):
+            x, y = vecdist(T, H)
+            T = vecsum(T, (x and abs(x)//x, y and abs(y)//y))
+        visited.add(T)
+
+print(len(visited))
--- py/9b.py
+++ py/9b.py
@@ -1,57 +1,22 @@
-visited = [] # set()
 dirs = {"R": (1, 0), "L": (-1, 0), "U": (0, 1), "D": (0, -1)}
 
 vecsum = lambda v1, v2: (v1[0]+v2[0], v1[1]+v2[1])
-def vecdist(v1, v2):
-    xd = v2[0]-v1[0]
-    yd = v2[1]-v1[1]
-    return (xd, yd)
-    
-def vecabs(v):
-    return (abs(v[0]), abs(v[1]))
+vecdist = lambda v1, v2: (v2[0]-v1[0], v2[1]-v1[1])
    
-def toofar(v): # returns 1 for 2-space normal case, 2 for diagonal move required
-    v = vecabs(v)
-    if v[0]==v[1]: return abs(v[0])
-    return sum(v)
-
-#test
-h = (2,4)
-t = (2,2)
-print(vecdist(h,t))
-print(toofar(vecdist(h,t)))
-
+def toofar(dist):
+    v = abs(dist[0]), abs(dist[1])
+    return (v[0]==v[1] and abs(v[0]) or sum(v)) > 1
 
 K = [(0, 0) for i in range(10)]
+visited = set()
 for line in open("9.input").readlines():
-    d = line[0]
-    print(line)
-    moves = int(line.strip()[2:])
-    for i in range(moves):
-        K[0] = vecsum(K[0], dirs[d]) # head moves
-        for jj in range(1, len(K)):
-            if f := toofar(vecdist(K[jj], K[jj-1])):
-                #if f == 1:
-                    #T = vecsum(T, dirs[d])
-                if f > 1:
-                    x,y=vecdist(K[jj], K[jj-1])
-                    if x==0 or y==0:
-                        if x==0:
-                            K[jj] = vecsum(K[jj], (0, abs(y)//y))
-                        elif y==0:
-                            K[jj] = vecsum(K[jj], (abs(x)//x, 0))
-                    elif abs(x)==1 and abs(y)==2:
-                        K[jj] = vecsum(K[jj], (x, abs(y)//y))
-                    elif abs(x)==2 and abs(y)==1:
-                        K[jj] = vecsum(K[jj], (abs(x)//x, y))
-                    elif abs(x)==2 and abs(y)==2:
-                        K[jj] = vecsum(K[jj], (abs(x)//x, abs(y)//y))
-                    else:
-                        print(x,y)
-                        raise Exception("unexpected diag move")
-            visited.append(K[-1])
-        print(K, len(set(visited)))
-   
-    
-    
-print(len(set(visited)))
+    for _move in range(int(line[2:])):
+        K[0] = vecsum(K[0], dirs[line[0]])
+        for i in range(1, len(K)):
+            dist = vecdist(K[i], K[i-1])
+            if toofar(dist):
+                x, y = dist
+                K[i] = vecsum(K[i], (x and abs(x)//x, y and abs(y)//y))
+            visited.add(K[-1])
+
+print(len(visited))
