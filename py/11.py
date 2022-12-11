98faef885b6828a5f2185cf192d00a9ea5f2b45b
Joonas Hiltunen <joonas.hiltunen@gmail.com>
2022-12-11 10:39:30 +0200

    [11] Clean solutions a bit

--- py/11.py
+++ py/11.py
@@ -9,13 +9,6 @@ class Monkey:
         self.inspections += 1
         
 monkeys = []  
-# EXAMPLE MONKEYS
-#monkeys.append(Monkey(lambda x: x*19, lambda x: x%23==0 and 2 or 3,  [79, 98])) # 0
-#monkeys.append(Monkey(lambda x: x+6,  lambda x: x%19==0 and 2 or 0,  [54, 65, 75, 74])) # 1
-#monkeys.append(Monkey(lambda x: x*x,  lambda x: x%13==0 and 1 or 3,  [79, 60, 97])) # 2
-#monkeys.append(Monkey(lambda x: x+3,  lambda x: x%17 and 1 or 0,     [74])) # 3
-
-# REAL MONKEYS
 monkeys.append(Monkey(lambda x: x*3,  lambda x: x%2==0 and 1 or 4,  [66, 59, 64, 51])) # 0
 monkeys.append(Monkey(lambda x: x*19, lambda x: x%7==0 and 3 or 5,  [67, 61])) # 1
 monkeys.append(Monkey(lambda x: x+2,  lambda x: x%11==0 and 4 or 0, [86, 93, 80, 70, 71, 81, 56])) # 2
@@ -26,15 +19,12 @@ monkeys.append(Monkey(lambda x: x+7,  lambda x: x%17==0 and 7 or 2, [82, 98, 77,
 monkeys.append(Monkey(lambda x: x+4,  lambda x: x%13==0 and 2 or 0, [54, 95, 70, 93, 88, 93, 63, 50])) # 7
 
 for rd in range(20):
-    for im, monkey in enumerate(monkeys):
+    for monkey in monkeys:
         while monkey.items:
-            # op -> x // 3 -> test
-            item = monkey.items.pop(0)
-            item = monkey.op(item)
+            item = monkey.op(monkey.items.pop(0))
             monkey.inspected()
             item //= 3
             monkeys[monkey.test(item)].items.append(item)
-        print([m.items for m in monkeys])
 
 r = sorted([m.inspections for m in monkeys])[-2:]
-print(r[-1]*r[-2])
+print(r[0]*r[1])
